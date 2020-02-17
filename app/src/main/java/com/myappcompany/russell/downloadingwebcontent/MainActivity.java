84db6b135c0c99c16ac9a97678657ba71073704a
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-17 11:23:57 -1000

    able to download html web content

--- app/src/main/java/com/myappcompany/russell/downloadingwebcontent/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/downloadingwebcontent/MainActivity.java
@@ -5,16 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
-        protected String doInBackground(String... strings) {
+        protected String doInBackground(String... urls) {
 
-            Log.i("URL", strings[0]);
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
 
-            return "Done";
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                return "Failed";
+            }
+
         }
     }
 
